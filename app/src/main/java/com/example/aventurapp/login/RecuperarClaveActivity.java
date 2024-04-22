72f34da3cdf0b62389e7f1d850265f79892a3f94
Gabriel <gabriel.lescano08@gmail.com>
2024-04-21 21:00:45 -0300

    Agregado configuración de conexión http y https

---
+++ app/src/main/java/com/example/aventurapp/login/RecuperarClaveActivity.java
@@ -0,0 +1,22 @@
+package com.example.aventurapp.login;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.aventurapp.R;
+
+public class RecuperarClaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_recuperar_clave);
+
+    }
+}
