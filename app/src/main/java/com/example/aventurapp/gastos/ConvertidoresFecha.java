981bfc42c7772aea09b37883d8d30fe7caa96569
Gabriel <gabriel.lescano08@gmail.com>
2024-05-19 20:19:33 -0300

    Modificado Geolocalización (Verificaciones)

--- app/src/main/java/com/example/aventurapp/gastos/ConvertidoresFecha.java
+++ app/src/main/java/com/example/aventurapp/gastos/ConvertidoresFecha.java
@@ -4,6 +4,12 @@ import androidx.room.TypeConverter;
 
 import java.util.Date;
 
+
+// Esta clase se utiliza para convertir entre objetos Date y valores de tiempo
+// en formato Long
+
+//Los métodos están anotados con @TypeConverter, lo que sugiere que se utilizan
+//con Room ya que admite persistencia de objetos para convertir entre tipos que la bd pueda almacenar
 public class ConvertidoresFecha {
     @TypeConverter
     public static Date fromTimestamp(Long value){
